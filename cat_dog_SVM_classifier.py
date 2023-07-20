9fe17615857a8d30a30b1a5c121b607761a31f45
Anh Phan <anhphanvt2705@gmail.com>
2023-07-20 10:48:02 +0700

    running SVM model

--- cat_dog_SVM_classifier.py
+++ cat_dog_SVM_classifier.py
@@ -3,15 +3,19 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torchvision import datasets, models, transforms
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 import numpy as np
 import os
+import time
+import pickle
 
 ## Define file directories
-file_dir = './data-shorten'
-output_dir = './output/VGG16_trained.pth'
-TRAIN = 'train' 
-VAL = 'val'
-TEST = 'test'
+file_dir = "./data-shorten"
+output_dir = "./output/VGG16_SVM_trained.pth"
+TRAIN = "train"
+VAL = "val"
+TEST = "test"
+
 
 def get_data(file_dir):
     """
@@ -32,37 +36,39 @@ def get_data(file_dir):
     print("[INFO] Loading data...")
     # Initialize data transformations
     data_transform = {
-        TRAIN: transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()
-        ]),
-        VAL: transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor()
-        ]),
-        TEST: transforms.Compose([
-            transforms.Resize(254),
-            transforms.CenterCrop(224),
-            transforms.ToTensor()
-        ])
+        TRAIN: transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+            ]
+        ),
+        VAL: transforms.Compose(
+            [
+                transforms.Resize(256), 
+                transforms.CenterCrop(224), 
+                transforms.ToTensor()
+            ]
+        ),
+        TEST: transforms.Compose(
+            [
+                transforms.Resize(254), 
+                transforms.CenterCrop(224), 
+                transforms.ToTensor()
+            ]
+        ),
     }
     # Initialize datasets and apply transformations
     datasets_img = {
         file: datasets.ImageFolder(
-            os.path.join(file_dir, file),
-            transform=data_transform[file]
+            os.path.join(file_dir, file), transform=data_transform[file]
         )
         for file in [TRAIN, VAL, TEST]
     }
     # Load data into dataloaders
     dataloaders = {
         file: torch.utils.data.DataLoader(
-            datasets_img[file],
-            batch_size=8,
-            shuffle=True,
-            num_workers=4
+            datasets_img[file], batch_size=8, shuffle=True, num_workers=4
         )
         for file in [TRAIN, VAL, TEST]
     }
@@ -76,7 +82,7 @@ def get_data(file_dir):
     return datasets_img, datasets_size, dataloaders, class_names
 
 
-def get_vgg16_pretrained_model(weights=models.VGG16_BN_Weights.DEFAULT):
+def get_vgg16_modified_model(weights=models.VGG16_BN_Weights.DEFAULT):
     """
     Retrieve the VGG-16 pre-trained model and remove the classifier layers.
 
@@ -98,62 +104,104 @@ def get_vgg16_pretrained_model(weights=models.VGG16_BN_Weights.DEFAULT):
     features = list(vgg16.classifier.children())[:-7]
     # Replace the model's classifier
     vgg16.classifier = nn.Sequential(*features)
-    print(vgg16)
+    # print(vgg16)
     return vgg16
 
-def get_features(vgg):
+
+def get_features(vgg, file=TRAIN):
+    print(f"[INFO] Getting '{file}' features...")
     svm_features = []
     svm_labels = []
-    
-    for i, data in enumerate(dataloaders[TRAIN]):
-        inputs, labels = data
+    data_batches_len = len(dataloaders[file])
+    for i, data_batch in enumerate(dataloaders[file]):
+        print(f"\r[FEATURE] Loading batch {i + 1}/{data_batches_len} ({len(data_batch[1])*(i+1)} images)", end='', flush=True)
+        # In this case, loaded databatch of 8 images including 8 features and 8 labels
+        inputs, labels = data_batch
         if use_gpu:
             # Get the data through the feature extractor of VGG16
             inputs = Variable(inputs.cuda())
             labels = Variable(labels.cuda())
             # Extract data from VGG16 feature extractor as a vector
             features = vgg(inputs)
-            features = features.cpu().detach().numpy().flatten()
+            # print(features.shape)     # torch.Size([8, 25088])
+            # print(labels.shape)       # torch.Size([8])
+            features = features.cpu().detach().numpy()
             labels = labels.cpu().detach().numpy()
-            # Add it to the features list
-            svm_features.append(features)
-            svm_labels.append(labels)
         else:
             # Get the data through the feature extractor of VGG16
             inputs = Variable(inputs)
             labels = Variable(labels)
             # Extract data from VGG16 feature extractor as a vector
             features = vgg(inputs)
-            features = features.detach().numpy().flatten()
-            labels = labels.cpu().detach().numpy()
-            # Add it to the features list
-            svm_features.append(features)
-            svm_labels.append(labels)
-        # features = np.ndarray(features)
-        # features.flatten()
+            features = features.detach().numpy()
+            labels = labels.detach().numpy()
         
+        # Add feature with correct label into an array
+        # print(features.shape)     # (8, 25088)
+        # print(labels.shape)       # (8,)
+        for index in range(len(labels)):
+            feature = features[index]  
+            label = labels[index]
+            # Add it to the features list
+            # print(feature.shape)     # (25088,)
+            svm_features.append(feature)
+            # print(label.shape)       # (1)
+            svm_labels.append(label)
+            
+    print("\n[FEATURE] Features loaded")
     return svm_features, svm_labels
 
-def svm_classifier(features, labels):
-    features = np.array(features)
-    labels = np.array(labels)
-    
-    
-    
-    return None
 
-if __name__ == '__main__':
-    # Use GPU if available
+def svm_classifier(train_data, test_data, epochs=5):
+    since = time.time()
+    FEATURE_INDEX = 0
+    LABEL_INDEX = 1
+    # There are 1000 images in the train data
+    train_features = np.array(train_data[FEATURE_INDEX])
+    # print(features.shape)     # (1000, 25088)
+    train_labels = np.array(train_data[LABEL_INDEX])
+    # print(labels.shape)       # (1000,)
+    
+    # There are 600 images in the test data
+    test_features = np.array(test_data[FEATURE_INDEX])
+    # print(features.shape)     # (1000, 25088)
+    test_labels = np.array(test_data[LABEL_INDEX])
+    # print(labels.shape)       # (1000,)
+    scores = []
+    svm_model = SVC(gamma="auto")
+    for epoch in range(epochs):
+        print(f"[SVM Model] Training epoch {epoch+1}/{epochs}...")
+        svm_model.fit(train_features, train_labels)
+        score = svm_model.score(test_features, test_labels)
+        print(f"[SVM Model] The score for epoch {epoch+1} is: {score:.4f}\n")
+        scores.append(score)
+    elapsed_time = time.time() - since
+    print(f"[INFO] Model has score mean: {np.mean(scores):.4f} and standard deviation: {np.std(scores):.4f} in {(elapsed_time // 60):.0f}m {(elapsed_time % 60):.0f}s")
+    return svm_model, np.mean(scores), np.std(scores)
+
+
+if __name__ == "__main__":
+    # Use GPU if available. Note that this only to load features using VGG16. Scikit Learn SVM does not support GPU
     use_gpu = torch.cuda.is_available()
     print("[INFO] Using CUDA") if use_gpu else print("[INFO] Using CPU")
     # Get Data
     datasets_img, datasets_size, dataloaders, class_names = get_data(file_dir)
     # Get VGG16 pre-trained model
-    vgg16 = get_vgg16_pretrained_model()
-    # vgg16 = get_vgg16_pretrained_model('./output/VGG16_trained.pth', len_target=2)      # If load custom pre-trained model, watch out to match len target
+    vgg16 = get_vgg16_modified_model()
     # Move model to GPU
     if use_gpu:
         torch.cuda.empty_cache()
         vgg16.cuda()
     # Extract features and labels from the VGG16 model
-    svm_features, svm_labels = get_features(vgg16)
+    svm_train_features, svm_train_labels = get_features(vgg16, TRAIN)
+    svm_test_features, svm_test_labels = get_features(vgg16, TEST)
+    # Run SVM 
+    svm_model, score_mean, score_stdv = svm_classifier(
+        [svm_train_features, svm_train_labels],
+        [svm_test_features, svm_test_labels],
+        epochs=5
+    )
+    # Save model
+    print('[INFO] Saving model...')
+    pickle.dump(svm_model, open(output_dir, 'wb'))
+    print('[INFO] Done')
